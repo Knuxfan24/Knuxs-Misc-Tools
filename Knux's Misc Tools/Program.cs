16f1d0aca6eaa4c4e9245d57781c69e2bdba6d3f
Knux <knuxfan@live.co.uk>
2022-03-04 18:45:22 +0000

    Wiped Program.cs
    Want to try and get into the habit of leaving it empty rather than having whatever dumb shit I last popped into it present.

--- Knux's Misc Tools/Program.cs
+++ Knux's Misc Tools/Program.cs
@@ -4,13 +4,6 @@
     {
         static void Main(string[] args)
         {
-            string[] matFiles = Directory.GetFiles(@"D:\Standalone Games\Big Rigs Racing\Dum\Data", "*.mat", SearchOption.AllDirectories);
-            foreach (string matFile in matFiles)
-            {
-                CarZ.MaterialLibrary mat = new();
-                mat.Load(matFile);
-                mat.Save($"{matFile}.knx");
-            }
         }
     }
 }
