7ba62f7404b376f417fc08558b1d65c14894288c
Knux <knuxfan@live.co.uk>
2022-04-27 14:17:12 +0100

    Remembered that I added the global usings to Program.cs
    Whoops.

--- Knux's Misc Tools/Program.cs
+++ Knux's Misc Tools/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Knuxs_Misc_Tools
+﻿global using Marathon.IO;
+global using System.Diagnostics;
+global using System.Numerics;
+
+namespace Knuxs_Misc_Tools
 {
     internal class Program
     {
