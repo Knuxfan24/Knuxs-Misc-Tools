003919027821d0cb29503e8338cfe933833f946f
Knux <knuxfan@live.co.uk>
2022-05-19 11:37:24 +0100

    (WoC) Use Marathon's Data Setup
    So I can dump it to JSON easily.

--- Knux's Misc Tools/WrathOfCortex/NUS.cs
+++ Knux's Misc Tools/WrathOfCortex/NUS.cs
@@ -6,25 +6,30 @@ namespace Knuxs_Misc_Tools.WrathOfCortex
 {
     public class NUS : FileBase
     {
+        public class FormatData
+        {
+            public List<string>? Names { get; set; }
+
+            public List<HGO_Chunk.Texture>? Textures { get; set; }
+
+            public List<HGO_Chunk.Material>? Materials { get; set; }
+
+            public List<HGO_Chunk.Geometry>? Geometry { get; set; }
+
+            public HGO_Chunk.INST? INST { get; set; }
+
+            public List<HGO_Chunk.SPECEntry>? SPEC { get; set; }
+
+            public List<HGO_Chunk.SSTEntry>? SST { get; set; }
+
+            public uint? LDirSize { get; set; }
+
+            public HGO_Chunk.TextureAnimation? TextureAnimations { get; set; }
+        }
+
         public override string Signature { get; } = "0CSG";
 
-        public List<string>? Names { get; set; }
-
-        public List<HGO_Chunk.Texture>? Textures { get; set; }
-
-        public List<HGO_Chunk.Material>? Materials { get; set; }
-
-        public List<HGO_Chunk.Geometry>? Geometry { get; set; }
-
-        public HGO_Chunk.INST? INST { get; set; }
-
-        public List<HGO_Chunk.SPECEntry>? SPEC { get; set; }
-
-        public List<HGO_Chunk.SSTEntry>? SST { get; set; }
-
-        public uint? LDirSize { get; set; }
-
-        public HGO_Chunk.TextureAnimation? TextureAnimations { get; set; }
+        public FormatData Data = new();
 
         public override void Load(Stream stream)
         {
@@ -44,47 +49,47 @@ namespace Knuxs_Misc_Tools.WrathOfCortex
                 {
                     case "LBTN":
                         HGO_Chunk.NameTable? nameTable = new();
-                        Names = nameTable.Read(reader);
+                        Data.Names = nameTable.Read(reader);
                         break;
 
                     case "0TST":
                         HGO_Chunk.TextureSet textureSet = new();
-                        Textures = textureSet.Read(reader);
+                        Data.Textures = textureSet.Read(reader);
                         break;
 
                     case "00SM":
                         HGO_Chunk.MaterialSet materialSet = new();
-                        Materials = materialSet.Read(reader);
+                        Data.Materials = materialSet.Read(reader);
                         break;
 
                     case "0TSG":
                         HGO_Chunk.GeometrySet geometrySet = new();
-                        Geometry = geometrySet.Read(reader);
+                        Data.Geometry = geometrySet.Read(reader);
                         break;
 
                     case "TSNI":
-                        INST = new();
-                        INST.Read(reader);
+                        Data.INST = new();
+                        Data.INST.Read(reader);
                         break;
 
                     case "CEPS":
                         HGO_Chunk.SPEC spec = new();
-                        SPEC = spec.Read(reader);
+                        Data.SPEC = spec.Read(reader);
                         break;
 
                     case "0TSS":
                         HGO_Chunk.SST sst = new();
-                        SST = sst.Read(reader);
+                        Data.SST = sst.Read(reader);
                         break;
 
                     case "RIDL":
-                        LDirSize = chunkSize; // Literally just a large chunk made entirely of nulls because yes?
+                        Data.LDirSize = chunkSize; // Literally just a large chunk made entirely of nulls because yes?
                         reader.JumpAhead(chunkSize);
                         break;
 
                     case "0SAT":
-                        TextureAnimations = new();
-                        TextureAnimations.Read(reader);
+                        Data.TextureAnimations = new();
+                        Data.TextureAnimations.Read(reader);
                         break;
 
                     default:
@@ -105,57 +110,57 @@ namespace Knuxs_Misc_Tools.WrathOfCortex
             StreamWriter writer = File.CreateText(filepath);
 
             // Loop through all the Geometry entries.
-            for (int i1 = 0; i1 < Geometry.Count; i1++)
+            for (int i1 = 0; i1 < Data.Geometry.Count; i1++)
             {
                 // Add a comment numbering this Geometry entry to make the OBJ a tiny bit more organised.
                 writer.WriteLine($"\n# geometry{i1}\n");
 
                 // Loop through the Meshes in this Geometry entry.
-                for (int i2 = 0; i2 < Geometry[i1].Meshes.Count; i2++)
+                for (int i2 = 0; i2 < Data.Geometry[i1].Meshes.Count; i2++)
                 {
                     // If there isn't a Primitive here, don't bother writing it.
-                    if (Geometry[i1].Meshes[i2].Primitive != null)
+                    if (Data.Geometry[i1].Meshes[i2].Primitive != null)
                     {
                         // Write the Vertex Positions, Texture Coordinates and Normals.
-                        for (int i = 0; i < Geometry[i1].Meshes[i2].Vertices.Count; i++)
+                        for (int i = 0; i < Data.Geometry[i1].Meshes[i2].Vertices.Count; i++)
                         {
-                            writer.WriteLine($"v {Geometry[i1].Meshes[i2].Vertices[i].Position.X} {Geometry[i1].Meshes[i2].Vertices[i].Position.Y} {Geometry[i1].Meshes[i2].Vertices[i].Position.Z}");
+                            writer.WriteLine($"v {Data.Geometry[i1].Meshes[i2].Vertices[i].Position.X} {Data.Geometry[i1].Meshes[i2].Vertices[i].Position.Y} {Data.Geometry[i1].Meshes[i2].Vertices[i].Position.Z}");
                         }
 
                         writer.WriteLine();
-                        for (int i = 0; i < Geometry[i1].Meshes[i2].Vertices.Count; i++)
+                        for (int i = 0; i < Data.Geometry[i1].Meshes[i2].Vertices.Count; i++)
                         {
-                            writer.WriteLine($"vt {Geometry[i1].Meshes[i2].Vertices[i].TextureCoordinates.X} {Geometry[i1].Meshes[i2].Vertices[i].TextureCoordinates.Y}");
+                            writer.WriteLine($"vt {Data.Geometry[i1].Meshes[i2].Vertices[i].TextureCoordinates.X} {Data.Geometry[i1].Meshes[i2].Vertices[i].TextureCoordinates.Y}");
                         }
 
                         writer.WriteLine();
-                        for (int i = 0; i < Geometry[i1].Meshes[i2].Vertices.Count; i++)
+                        for (int i = 0; i < Data.Geometry[i1].Meshes[i2].Vertices.Count; i++)
                         {
-                            writer.WriteLine($"vn {Geometry[i1].Meshes[i2].Vertices[i].Normals.X} {Geometry[i1].Meshes[i2].Vertices[i].Normals.Y} {Geometry[i1].Meshes[i2].Vertices[i].Normals.Z}");
+                            writer.WriteLine($"vn {Data.Geometry[i1].Meshes[i2].Vertices[i].Normals.X} {Data.Geometry[i1].Meshes[i2].Vertices[i].Normals.Y} {Data.Geometry[i1].Meshes[i2].Vertices[i].Normals.Z}");
                         }
 
                         // Write the object header.
                         writer.WriteLine();
                         writer.WriteLine($"o geometry{i1}mesh{i2}");
                         writer.WriteLine($"g geometry{i1}mesh{i2}");
-                        writer.WriteLine($"usemtl Material{Geometry[i1].Meshes[i2].MaterialIndex}");
+                        writer.WriteLine($"usemtl Material{Data.Geometry[i1].Meshes[i2].MaterialIndex}");
 
+                        // TODO: All of this is fucked and doesn't work right.
                         // Write the Triangle Strips if the type is 6.
-                        // TODO: This conversion is fucked.
-                        if (Geometry[i1].Meshes[i2].Primitive.Type == 6)
+                        if (Data.Geometry[i1].Meshes[i2].Primitive.Type == 6)
                         {
-                            for (int i = 0; i < Geometry[i1].Meshes[i2].Primitive.FaceIndices.Count - 2; i++)
+                            for (int i = 0; i < Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices.Count - 2; i++)
                             {
                                 if ((i & 1) > 0)
                                     writer.WriteLine($"f " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount} " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount} " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}");
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount} " +
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount} " +
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}");
                                 else
                                     writer.WriteLine($"f " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount} " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount} " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}");
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount} " +
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount} " +
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}");
                             }
                             writer.WriteLine();
                         }
@@ -163,18 +168,18 @@ namespace Knuxs_Misc_Tools.WrathOfCortex
                         // Write the standard Triangle List if the type is 5.
                         else
                         {
-                            for (int i = 0; i < Geometry[i1].Meshes[i2].Primitive.FaceIndices.Count - 2; i++)
+                            for (int i = 0; i < Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices.Count - 2; i++)
                             {
                                 writer.WriteLine($"f " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount} " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount} " +
-                                                     $"{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}");
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i] + VertexCount} " +
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 1] + VertexCount} " +
+                                                     $"{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}/{Data.Geometry[i1].Meshes[i2].Primitive.FaceIndices[i + 2] + VertexCount}");
                             }
                             writer.WriteLine();
                         }
 
                         // Increment VertexCount so we don't accidentally use Vertices from the wrong mesh.
-                        VertexCount += Geometry[i1].Meshes[i2].Vertices.Count;
+                        VertexCount += Data.Geometry[i1].Meshes[i2].Vertices.Count;
                     }
                 }
             }
@@ -192,32 +197,32 @@ namespace Knuxs_Misc_Tools.WrathOfCortex
             writer = File.CreateText($@"{Path.GetDirectoryName(filepath)}\{Path.GetFileNameWithoutExtension(filepath)}.mtl");
 
             // Loop through all the Geometry entries.
-            for (int i1 = 0; i1 < Geometry.Count; i1++)
+            for (int i1 = 0; i1 < Data.Geometry.Count; i1++)
             {
                 // Loop through the Meshes in this Geometry entry.
-                for (int i2 = 0; i2 < Geometry[i1].Meshes.Count; i2++)
+                for (int i2 = 0; i2 < Data.Geometry[i1].Meshes.Count; i2++)
                 {
                     // Don't bother writing this material if the Mesh calling it doesn't have any Primitive data.
-                    if (Geometry[i1].Meshes[i2].Primitive != null)
+                    if (Data.Geometry[i1].Meshes[i2].Primitive != null)
                     {
                         // Only write this material if we haven't already.
-                        if (!writtenMats.Contains(Geometry[i1].Meshes[i2].MaterialIndex))
+                        if (!writtenMats.Contains(Data.Geometry[i1].Meshes[i2].MaterialIndex))
                         {
                             // Mark this index as having been written.
-                            writtenMats.Add(Geometry[i1].Meshes[i2].MaterialIndex);
+                            writtenMats.Add(Data.Geometry[i1].Meshes[i2].MaterialIndex);
 
                             // Write a newmtl header.
-                            writer.WriteLine($"newmtl Material{Geometry[i1].Meshes[i2].MaterialIndex}");
+                            writer.WriteLine($"newmtl Material{Data.Geometry[i1].Meshes[i2].MaterialIndex}");
 
                             // Write the material colours.
-                            writer.WriteLine($"\tKd {Materials[(int)Geometry[i1].Meshes[i2].MaterialIndex].Colours.X} {Materials[(int)Geometry[i1].Meshes[i2].MaterialIndex].Colours.Y} {Materials[(int)Geometry[i1].Meshes[i2].MaterialIndex].Colours.Z}");
+                            writer.WriteLine($"\tKd {Data.Materials[(int)Data.Geometry[i1].Meshes[i2].MaterialIndex].Colours.X} {Data.Materials[(int)Data.Geometry[i1].Meshes[i2].MaterialIndex].Colours.Y} {Data.Materials[(int)Data.Geometry[i1].Meshes[i2].MaterialIndex].Colours.Z}");
                             
                             // Write a placeholder diffuse map entry if this material has one.
                             // Also includes a comment with the type.
-                            if (Materials[(int)Geometry[i1].Meshes[i2].MaterialIndex].BitmapIndex != -1)
+                            if (Data.Materials[(int)Data.Geometry[i1].Meshes[i2].MaterialIndex].BitmapIndex != -1)
                             {
-                                writer.WriteLine($"\t#Bitmap Type = 0x{Textures[Materials[(int)Geometry[i1].Meshes[i2].MaterialIndex].BitmapIndex].Type:X}");
-                                writer.WriteLine($"\tmap_Kd bitmap{Materials[(int)Geometry[i1].Meshes[i2].MaterialIndex].BitmapIndex}.png");
+                                writer.WriteLine($"\t#Bitmap Type = 0x{Data.Textures[Data.Materials[(int)Data.Geometry[i1].Meshes[i2].MaterialIndex].BitmapIndex].Type:X}");
+                                writer.WriteLine($"\tmap_Kd bitmap{Data.Materials[(int)Data.Geometry[i1].Meshes[i2].MaterialIndex].BitmapIndex}.png");
                                     
                             }
 
@@ -233,17 +238,15 @@ namespace Knuxs_Misc_Tools.WrathOfCortex
             #endregion
 
             #region Bitmap Exporting
-            // Not functional, outputs garbage.
-            for (int i = 0; i < Textures.Count; i++)
+            // Not functional, outputs garbage due to me not knowing how to use the Deswizzler.
+            for (int i = 0; i < Data.Textures.Count; i++)
             {
-                if (Textures[i].Type == 0x80)
+                if (Data.Textures[i].Type == 0x80)
                 {
-                    ImageDecoderDirectColor test = new(Textures[i].Data, (int)Textures[i].Width, (int)Textures[i].Height, new ColorCodecDXT1(Rainbow.ImgLib.Common.ByteOrder.BigEndian, (int)Textures[i].Width, (int)Textures[i].Height));
-                    var wut = test.DecodeImage();
-                    using (var outputStream = new SKFileWStream($@"{Path.GetDirectoryName(filepath)}\bitmap{i}.png"))
-                    {
-                        SKPixmap.Encode(outputStream, wut, SKEncodedImageFormat.Png, 100);
-                    }
+                    SKBitmap? decodedBitmap = new ImageDecoderDirectColor(Data.Textures[i].Data, (int)Data.Textures[i].Width, (int)Data.Textures[i].Height, new ColorCodecDXT1(Rainbow.ImgLib.Common.ByteOrder.BigEndian, (int)Data.Textures[i].Width, (int)Data.Textures[i].Height)).DecodeImage();
+
+                    using (SKFileWStream? outputStream = new($@"{Path.GetDirectoryName(filepath)}\bitmap{i}.png"))
+                        SKPixmap.Encode(outputStream, decodedBitmap, SKEncodedImageFormat.Png, 100);
                 }
             }
 
