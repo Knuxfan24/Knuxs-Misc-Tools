a9137f4ffcd882696afb6194c679ff70f65cca32
Knux <knuxfan@live.co.uk>
2022-05-21 18:17:05 +0100

    (WoC) .AI (entity table) reading and writing
    Because it's a simple format that was basically already documented here: https://github.com/Open-Travelers/LibTWOC/blob/main/Serializable%20Objects/DOCS.md

---
+++ Knux's Misc Tools/WrathOfCortex/EntityTable.cs
@@ -0,0 +1,49 @@
+﻿namespace Knuxs_Misc_Tools.WrathOfCortex
+{
+    public class EntityTable : FileBase
+    {
+        public List<Entity> Entities = new();
+        
+        public override void Load(Stream stream)
+        {
+            BinaryReaderEx reader = new(stream, true);
+
+            uint entityCount = reader.ReadUInt32();
+
+            for (int i = 0; i < entityCount; i++)
+            {
+                Entity entry = new();
+                entry.Type = reader.ReadNullPaddedString(0x10);
+                uint positionCount = reader.ReadUInt32();
+
+                for (int e = 0; e < positionCount; e++)
+                    entry.Positions.Add(reader.ReadVector3());
+
+                Entities.Add(entry);
+            }
+        }
+
+        public override void Save(Stream stream)
+        {
+            BinaryWriterEx writer = new(stream, true);
+
+            writer.Write(Entities.Count);
+
+            foreach (Entity? entityGroup in Entities)
+            {
+                writer.WriteNullPaddedString(entityGroup.Type, 0x10);
+                writer.Write(entityGroup.Positions.Count);
+
+                foreach (var entity in entityGroup.Positions)
+                    writer.Write(entity);
+            }
+        }
+    }
+
+    public class Entity
+    {
+        public string Type { get; set; }
+
+        public List<Vector3> Positions { get; set; } = new();
+    }
+}
