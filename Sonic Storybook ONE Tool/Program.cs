5204279510c7a74667c961f614165c5c76c48662
Knux <knuxfan@live.co.uk>
2022-08-04 00:21:50 +0100

    (Storybook) Made a ONE tool because yes

---
+++ Sonic Storybook ONE Tool/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Sonic_Storybook_ONE_Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage:\nSonic_Storybook_ONE_Tool.exe \"file.one\"\nSonic_Storybook_ONE_Tool.exe \"some_directory_path\"\n\nPress any key to continue...");
+                Console.ReadKey();
+            }
+
+            else
+            {
+                foreach (string file in args)
+                {
+                    if (Path.GetExtension(file) is ".one")
+                    {
+                        Directory.CreateDirectory(@$"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}");
+                        Knuxs_Misc_Tools.Storybook.ONE archive = new();
+                        archive.Load(file, true, @$"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}");
+                    }
+
+                    if (Directory.Exists(file))
+                    {
+                        Knuxs_Misc_Tools.Storybook.ONE archive = new();
+                        archive.GetFiles(file);
+                        archive.Save($"{file}.one");
+                    }
+                }
+            }
+        }
+    }
+}
