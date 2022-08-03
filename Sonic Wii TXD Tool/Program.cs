5b9353e592cef0c24757531ce321ffd9c8e99f99
Knux <knuxfan@live.co.uk>
2022-08-04 00:28:48 +0100

    TXD Tool for SWA_Wii and Storybook
    Believe the TXD format is the same between them at least.

---
+++ Sonic Wii TXD Tool/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Sonic_Wii_TXD_Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage:\nSonic_Wii_TXD_Tool.exe \"file.txd\"\nSonic_Wii_TXD_Tool.exe \"some_directory_path\"\n\nPress any key to continue...");
+                Console.ReadKey();
+            }
+
+            else
+            {
+                foreach (string file in args)
+                {
+                    if (Path.GetExtension(file) is ".txd")
+                    {
+                        Directory.CreateDirectory(@$"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}");
+                        Knuxs_Misc_Tools.Storybook.TXD textures = new();
+                        textures.Load(file);
+                        textures.Extract(@$"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}");
+                    }
+
+                    if (Directory.Exists(file))
+                    {
+                        Knuxs_Misc_Tools.Storybook.TXD textures = new();
+                        textures.GetFiles(file);
+                        textures.Save($"{file}.txd");
+                    }
+                }
+            }
+        }
+    }
+}
