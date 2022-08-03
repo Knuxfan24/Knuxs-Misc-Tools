b08a6c1e7c8bc18f77f1db13cfee5c1df33528ca
Knux <knuxfan@live.co.uk>
2022-08-03 06:01:19 +0100

    I love one unique value

--- Knux's Misc Tools/Storybook/SET.cs
+++ Knux's Misc Tools/Storybook/SET.cs
@@ -8,7 +8,7 @@
 
             public Vector3 Rotation { get; set; } // TODO: Verify if the three values after position are actually rotation in BAMs format.
 
-            public byte UnknownByte_1 { get; set; } // TODO: Find out what this is.
+            public byte UnknownByte_1 { get; set; } // Has three values, 01 seems to imply no parameters, 09 is the most comman and 0D shows up in one object.
 
             public byte UnknownByte_2 { get; set; } // TODO: Find out what this is.
 
