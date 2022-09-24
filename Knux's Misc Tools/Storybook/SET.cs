ee18160d24df8f9806feeb976dc089ffc74fead5
Knux <knuxfan@live.co.uk>
2022-09-24 06:00:11 +0100

    (Storybook) Dumb SET Parameter stuff
    Doing this properly would really require finding it in the executable. Which is... Yikes. Nah. Not for me.

--- Knux's Misc Tools/Storybook/SET.cs
+++ Knux's Misc Tools/Storybook/SET.cs
@@ -1,6 +1,4 @@
-﻿using HedgeLib.Sets;
-
-namespace Knuxs_Misc_Tools.Storybook
+﻿namespace Knuxs_Misc_Tools.Storybook
 {
     internal class SET : FileBase
     {
@@ -32,14 +30,18 @@ namespace Knuxs_Misc_Tools.Storybook
 
             public uint UnknownUInt32_2 { get; set; } // TODO: Find out what this is.
 
-            // TODO: Better way to handle parameters, if there was a data type indicator in there that'd be lovely...
-            public List<uint>? Parameters { get; set; } 
-            public List<float>? ParametersF { get; set; }
-            public List<byte>? ParametersB { get; set; }
+            public List<SetParameter> Parameters { get; set; } = new();
 
             public string? Name { get; set; } // Not actually a thing these have, filled in by function for ease of research.
 
             public override string ToString() => Name;
+        }
+
+        public class SetParameter
+        {
+            public object? Data { get; set; }
+
+            public Type? DataType { get; set; }
         }
 
         public class FormatData
@@ -111,28 +113,14 @@ namespace Knuxs_Misc_Tools.Storybook
                     reader.JumpAhead(0x7);
                 }
 
-                // Calculate how many parameters this object has.
-                int objectParameterCount = reader.ReadByte() / 4;
+                // Calculate how many bytes make up the parameters for this object.
+                int objectParameterLength = reader.ReadByte();
 
                 // Jump to the apporiate location in the parameter data table.
                 reader.JumpTo(parameterDataTableOffset + parameterOffset);
 
                 // Read the parameters.
-                // TODO: Actually handle their data types. I have no clue if that's even in here (like in '06) or not...
-                obj.Parameters = new();
-                obj.ParametersF = new();
-                obj.ParametersB = new();
-                for (int p = 0; p < objectParameterCount; p++)
-                {
-                    obj.Parameters.Add(reader.ReadUInt32());
-                    reader.JumpBehind(0x4);
-                    obj.ParametersF.Add(reader.ReadSingle());
-                    reader.JumpBehind(0x4);
-                    obj.ParametersB.Add(reader.ReadByte());
-                    obj.ParametersB.Add(reader.ReadByte());
-                    obj.ParametersB.Add(reader.ReadByte());
-                    obj.ParametersB.Add(reader.ReadByte());
-                }
+                ReadParameters(reader, objectParameterLength, obj);
 
                 // Jump back for our next object.
                 reader.JumpTo(pos);
@@ -140,6 +128,58 @@ namespace Knuxs_Misc_Tools.Storybook
                 // Save this object.
                 Data.Objects.Add(obj);
             }
+        }
+
+        // Maybe replace this with a template system rather than hardcoding all the types in here? Not sure how I'd do that personally.
+        // TODO: Literally every object...
+        private static void ReadParameters(BinaryReaderEx reader, int objectParameterLength, SetObject obj)
+        {
+            switch (obj.Table, obj.ID)
+            {
+                // RING
+                case (0x00, 0x01):
+                    obj.Parameters.Add(ReadParameter(reader, typeof(byte)));  // Type. 0 = single?, 1 = default, 2 = single that gives RingCount, 3 = same as 2?, 4 = same as 1?, 5 = vertical stack
+                    obj.Parameters.Add(ReadParameter(reader, typeof(byte)));  // RingCount.
+                    obj.Parameters.Add(ReadParameter(reader, typeof(byte)));  // Something to do with the pattern of the trail?
+                    obj.Parameters.Add(ReadParameter(reader, typeof(bool)));  // IsSilverRing.
+                    obj.Parameters.Add(ReadParameter(reader, typeof(float))); // Spacing.
+                    obj.Parameters.Add(ReadParameter(reader, typeof(float))); // Unknown.
+                    obj.Parameters.Add(ReadParameter(reader, typeof(float))); // Unknown.
+                    break;
+
+                // DASH_PANEL
+                case (0x00, 0x13):
+                    obj.Parameters.Add(ReadParameter(reader, typeof(float))); // Speed?
+                    obj.Parameters.Add(ReadParameter(reader, typeof(uint)));  // OutOfControl in frames?
+                    break;
+
+                // INVISIBLE_COLLISION
+                //case (0x00, 0x55):
+                //    four shorts?
+                //    break;
+
+                default:
+                    for (int i = 0; i < objectParameterLength; i++)
+                        obj.Parameters.Add(ReadParameter(reader, typeof(byte)));
+                    break;
+            }
+        }
+
+        private static SetParameter ReadParameter(BinaryReaderEx reader, Type type)
+        {
+            SetParameter parameter = new() { DataType = type };
+
+            switch (type.ToString())
+            {
+                case "System.Byte": parameter.Data = reader.ReadByte(); break;
+                case "System.Boolean": parameter.Data = reader.ReadBoolean(); break;
+                case "System.Single": parameter.Data = reader.ReadSingle(); break;
+                case "System.UInt32": parameter.Data = reader.ReadUInt32(); break;
+
+                default: throw new NotImplementedException($"Data type of {type} not handled");
+            }
+
+            return parameter;
         }
 
         public override void Save(Stream stream)
@@ -187,9 +227,25 @@ namespace Knuxs_Misc_Tools.Storybook
             {
                 if (Data.Objects[i].UnknownByte_1 != 0x1)
                 {
+                    // Calculate the parameter length for this object.
+                    byte parameterLength = 0;
+                    foreach (SetParameter parameter in Data.Objects[i].Parameters)
+                    {
+                        switch (parameter.DataType.ToString())
+                        {
+                            case "System.Byte": parameterLength++; break;
+                            case "System.Boolean": parameterLength++; break;
+                            case "System.Single": parameterLength += 4; break;
+                            case "System.UInt32": parameterLength += 4; break;
+
+                            default:
+                                throw new NotImplementedException($"Data type of {parameter.DataType} not handled");
+                        }
+                    }
+
                     writer.Write((byte)0x1);
                     writer.Write((byte)0x0);
-                    writer.Write((byte)(Data.Objects[i].Parameters.Count * 4));
+                    writer.Write(parameterLength);
                     writer.WriteNulls(0x5);
                 }
             }
@@ -202,22 +258,18 @@ namespace Knuxs_Misc_Tools.Storybook
             {
                 if (Data.Objects[i].UnknownByte_1 != 0x1)
                 {
-                    if (Data.Objects[i].ID != 0x01 && Data.Objects[i].Table != 0x00)
+                    foreach (SetParameter parameter in Data.Objects[i].Parameters)
                     {
-                        foreach (var parameter in Data.Objects[i].Parameters)
+                        switch (parameter.DataType.ToString())
                         {
-                            writer.Write(parameter);
+                            case "System.Byte": writer.Write((byte)parameter.Data); break;
+                            case "System.Boolean": writer.Write((bool)parameter.Data); break;
+                            case "System.Single": writer.Write((float)parameter.Data); break;
+                            case "System.UInt32": writer.Write((uint)parameter.Data); break;
+
+                            default:
+                                throw new NotImplementedException($"Data type of {parameter.DataType} not handled");
                         }
-                    }
-                    else
-                    {
-                        writer.Write(Data.Objects[i].ParametersB[0]);
-                        writer.Write(Data.Objects[i].ParametersB[1]);
-                        writer.Write(Data.Objects[i].ParametersB[2]);
-                        writer.Write(Data.Objects[i].ParametersB[3]);
-                        writer.Write(Data.Objects[i].ParametersF[1]);
-                        writer.Write(Data.Objects[i].Parameters[2]);
-                        writer.Write(Data.Objects[i].Parameters[3]);
                     }
                 }
             }
@@ -247,39 +299,6 @@ namespace Knuxs_Misc_Tools.Storybook
                 foreach (SETItems.ObjectEntry entry in items.Data.Objects)
                     if (entry.Table == obj.Table && entry.ID == obj.ID)
                         obj.Name = entry.Name;
-        }
-
-        public void DumpGLVL(string filepath)
-        {
-            // Create the Generations SET File.
-            GensSetData set = new();
-
-            // Loop through each object.
-            for (int i = 0; i < Data.Objects.Count; i++)
-            {
-                // Create a Generations object.
-                HedgeLib.Sets.SetObject gensObj = new();
-                gensObj.Transform.Position = new(Data.Objects[i].Position.X / 10, Data.Objects[i].Position.Y / 10, Data.Objects[i].Position.Z / 10);
-                var rot = Helpers.ConvertToQuat(Data.Objects[i].Rotation.Y);
-                gensObj.Transform.Rotation = new(rot.X, rot.Y, rot.Z, rot.W);
-                gensObj.ObjectID = (uint)i;
-
-                // Add dummy unknown objects.
-                if (Data.Objects[i].Name != null)
-                    gensObj.ObjectType = $"{Data.Objects[i].Name.Replace(' ', '_')}";
-                else
-                    gensObj.ObjectType = $"Unknown_Table{Data.Objects[i].Table}Index{Data.Objects[i].ID}";
-
-                if (Data.Objects[i].Parameters != null)
-                    for (int p = 0; p < Data.Objects[i].Parameters.Count; p++)
-                        gensObj.Parameters.Add(new SetObjectParam(typeof(int), Data.Objects[i].Parameters[p]));
-
-                // Save this object.
-                set.Objects.Add(gensObj);
-            }
-
-            // Save the SET file.
-            set.Save(filepath, true);
         }
     }
 }
