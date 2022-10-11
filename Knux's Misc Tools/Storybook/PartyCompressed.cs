c0abe67ec854e6efc5cdb25d6daea188aaf7206b
Knux <knuxfan@live.co.uk>
2022-10-11 15:41:38 +0100

    (Storybook) The most basic outline of the Party Mode archives
    We have no clue what the actual compression is.

---
+++ Knux's Misc Tools/Storybook/PartyCompressed.cs
@@ -0,0 +1,30 @@
+﻿namespace Knuxs_Misc_Tools.Storybook
+{
+    internal class PartyCompressed : FileBase
+    {
+        public override void Load(Stream stream)
+        {
+            BinaryReaderEx reader = new(stream, true);
+            reader.JumpAhead(0x4); // Always 1.
+            reader.IsBigEndian = false;
+            uint fileCount = reader.ReadUInt32();
+            reader.IsBigEndian = true;
+            uint unknownUInt32_1 = reader.ReadUInt32(); // Seems to match the number in the file name if there is one?
+
+            for (int i = 0; i < fileCount; i++)
+            {
+                uint fileOffset = reader.ReadUInt32();
+                long pos = reader.BaseStream.Position;
+                reader.JumpTo(fileOffset);
+                string compressionIndicator = reader.ReadNullTerminatedString();
+                reader.JumpAhead(0x2); // Always 40 00.
+                uint unknownUInt32_2 = reader.ReadUInt32();
+                uint unknownByte_1 = reader.ReadByte();
+                // TODO: What the hell is this compression then? Are file names a thing in it?
+
+                reader.JumpTo(pos);
+            }
+
+        }
+    }
+}
